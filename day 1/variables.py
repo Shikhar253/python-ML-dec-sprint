2cd348cf85d750c458da57df2dd7dfcff944522e
Shikhar Saxena <saxenashikhar01@gmail.com>
2025-12-03 05:33:19 -0500

    variables

---
+++ day 1/variables.py
@@ -0,0 +1,13 @@
+name = "Shikhar"
+age = 24
+is_motivated = True
+target_skills = ["Python", "ML", "DL", "MLOps"]  #list of strings
+dict_example={
+    "name": name,
+    "age": age,
+    "is_motivated": is_motivated,
+    "target_skills": target_skills,
+    "sprint_goal": "full python ds ML job ready"
+}
+
+
